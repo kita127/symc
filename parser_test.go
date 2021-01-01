922202c8b4aa83c7a17fe436b8967239d447fd13
kita127 <kitada.kosuke@gmail.com>
2021-01-01 15:02:42 +0900

    update

--- parser_test.go
+++ parser_test.go
@@ -489,23 +489,23 @@ void func(int a)
 				},
 			},
 		},
-		//		{
-		//			"function def 12",
-		//			`
-		//void func(int a)
-		//{
-		//    hoge = (unsigned char)10;
-		//}
-		//`,
-		//			&Module{
-		//				[]Statement{
-		//					&FunctionDef{Name: "func",
-		//						Params:     []*VariableDef{{Name: "a"}},
-		//						Statements: []Statement{&AccessVar{Name: "hoge"}},
-		//					},
-		//				},
-		//			},
-		//		},
+		{
+			"function def 12",
+			`
+void func(int a)
+{
+    hoge = (unsigned char)10;
+}
+`,
+			&Module{
+				[]Statement{
+					&FunctionDef{Name: "func",
+						Params:     []*VariableDef{{Name: "a"}},
+						Statements: []Statement{&AccessVar{Name: "hoge"}},
+					},
+				},
+			},
+		},
 		//		{
 		//			"function def 3",
 		//			`
